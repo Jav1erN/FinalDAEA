85bf09a8679251317b67f6e7281dee8ab9d8b264
Thefa <mireya.apaza@tecsup.edu.pe>
2026-05-28 18:30:24 -0500

    dependency injection

---
+++ ClinicSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ClinicSystem.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClinicSystem.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ClinicDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
